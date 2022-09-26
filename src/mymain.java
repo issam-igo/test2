2a593dea6c4ec84923b91a9b33ed616afc283650
issam igout <igoussam@gmail.com>
2022-09-25 20:58:35 -0400

    first commit

---
+++ src/mymain.java
@@ -0,0 +1,16 @@
+
+public class mymain {
+	
+	public static void main(String[] args) {
+		
+		mymain c=new mymain();
+		
+		System.out.println(c.calculer(2));
+	}
+	
+	public int calculer(int x) {
+		
+		return x*2;
+		
+	}
+}
