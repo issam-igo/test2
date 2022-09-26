4708a1fc1e47f1d85ed7956f8c1795f06a654a63
Ayoub <ayoub.azza@outlook.com>
2022-09-25 21:28:01 -0400

    a

--- src/mymain.java
+++ src/mymain.java
@@ -6,11 +6,18 @@ public class mymain {
 		mymain c=new mymain();
 		
 		System.out.println(c.calculer(2));
+		
+		System.out.println(c.division(2));
 	}
 	
 	public int calculer(int x) {
 		
 		return x*2;
 		
+	}
+	
+	public int division(int x) {
+		
+		return x/2;
 	}
 }
